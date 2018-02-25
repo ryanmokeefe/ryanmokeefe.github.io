f3233cb010c1b18c99dc207e6cabaf41286d26ad
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-02-25 01:23:47 -0500

    new

--- main.js
+++ main.js
@@ -3,14 +3,21 @@ console.log("hello world")
 $(function() {
     var div = $('.project-img');
     var width = div.width();
-    
+    // var solo = $(".project-solo")
     div.css('height', width);
 });
+
+// $(function() {
+//     var div = $('.project-img');
+//     var width = div.width();
+//     var solo = $(".project-solo")
+//     solo.css('height', width);
+// });
 
 $(function() {
     setTimeout(() => {
         var bird = $(".bird")[0];
         console.log(bird)
         bird.parentNode.removeChild(bird);
-    }, 10000);
+    }, 15000);
 })

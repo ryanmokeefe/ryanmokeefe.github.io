b1126ee33324e7791a5f44f713e7854dfb8a4dc0
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-07-30 02:25:50 -0400

    wip

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("hello world")
+// console.log("hello world")
 
 $(function() {
     var div = $('.project-img');
@@ -6,3 +6,37 @@ $(function() {
     div.css('height', width);
 });
 
+
+// Fixed NavBar
+
+
+var offsetNav = $(".nav").offset().top;
+$(window).resize(function(){
+    offsetNav = $(".nav").offset().top;
+})
+
+$(window).scroll(function() {
+    
+    var scrolltop = $(window).scrollTop();
+    var dist = (offsetNav - scrolltop);
+
+    if (dist <= 0)
+    {
+        $(".nav").removeClass("scrollNav");
+        $(".nav").addClass("fixedNav");
+    } else 
+    {
+        $(".nav").removeClass("fixedNav");
+        $(".nav").addClass("scrollNav");
+    }
+});
+
+
+
+
+// Nav Scrolling Links:
+
+
+// find dist from projects Top, contacts Top
+// make nav Buttons
+// on click, scroll to dist - navbar height

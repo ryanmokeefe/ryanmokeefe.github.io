f5c8d485f7c12f99ac1a008d21fe8c1bdc7ff8e2
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 12:31:49 -0400

    backup

--- main.js
+++ main.js
@@ -27,6 +27,45 @@ $(".scrollToTop").click(function() {
       }, 500);
 });
 
+//    Scroll to navLink
+
+// Snippet - Chris Coyier
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 // Fixed NavBar
 
@@ -50,6 +89,25 @@ $(window).scroll(function() {
         $(".nav").addClass("scrollNav");
     }
 });
+
+
+
+    // Nav Scroll Transition
+
+    // href="#
+    // $('a[href*="#"]')
+    
+    //     .click(function(e) {
+    //         e.preventDefault();
+    //         var target = this.href;
+    //         // scrollTo = target.offset().top;
+    //         // console.log(target);
+    //         $('body').animate({
+    //             scrollTop: 0
+    //           }, 500);
+    
+    //     });
+    
 
 
 // // Popout Info:
